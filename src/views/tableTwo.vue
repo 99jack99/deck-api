3ffeac166e96a60392dbd1c8d7f59ec8a1e1061e
9jack9 <jackson55adl@gmail.com>
2023-06-01 13:55:08 +0200

    added logic to table 2

--- src/views/tableTwo.vue
+++ src/views/tableTwo.vue
@@ -1,4 +1,97 @@
-<script setup></script>
+<script setup>
+import axios from "axios";
+import { ref, reactive, onBeforeMount } from "vue";
+import { toast } from "vue3-toastify";
+
+const url = "https://deckofcardsapi.com";
+
+let inferno_deck = reactive({
+  id: null,
+  remaining: null,
+});
+
+let owned_cards_inf = ref([]);
+
+const get_inferno_deck = () => {
+  axios
+    .get(`${url}/api/deck/new/shuffle/?deck_count=3`)
+
+    .then((res) => {
+      inferno_deck.id = res.data.deck_id;
+      inferno_deck.remaining = res.data.remaining;
+
+      localStorage.setItem("inferno_id", inferno_deck.id);
+      localStorage.setItem("inferno_remaining", inferno_deck.remaining);
+    })
+
+    .catch((error) => {
+      toast.error("Ups, could not get Inferno deck", {
+        position: toast.POSITION.BOTTOM_CENTER,
+      });
+      console.log(error);
+    });
+};
+
+const shuffle_cards = (deck_id) => {
+  axios
+    .get(`${url}/api/deck/${deck_id}/shuffle/`)
+    .then((res) => {
+      toast.success("Cards have been shuffled", {
+        position: toast.POSITION.BOTTOM_CENTER,
+      });
+      console.log(res);
+    })
+    .catch((res) => {
+      toast.error("Ups, there was a problem shuffling the deck", {
+        position: toast.POSITION.BOTTOM_CENTER,
+      });
+      console.log(res);
+    });
+};
+
+const draw_card = (deck_var, storage_name, deck_id, cards_drawn) => {
+  axios
+    .get(`${url}/api/deck/${deck_id}/draw/?count=${cards_drawn}`)
+    .then((res) => {
+      console.log(res.data.cards);
+      deck_var.remaining = res.data.remaining;
+      localStorage.setItem(`${storage_name}`, deck_var.remaining);
+      let data = res.data.cards;
+      if (owned_cards_inf.value.length <= 5) {
+        for (const cards of data) {
+          owned_cards_inf.value.push(cards);
+          localStorage.setItem(
+            "player_cards_inferno",
+            JSON.stringify(owned_cards_inf.value)
+          );
+        }
+      } else {
+        toast.error("Only 6 cards allowed in INFERNO LEVEL!", {
+          position: toast.POSITION.BOTTOM_CENTER,
+        });
+      }
+    })
+    .catch((res) => {
+      console.log(res);
+    });
+};
+
+onBeforeMount(() => {
+  /* In case player had cads in hand */
+  if (localStorage.getItem("player_cards_inferno")) {
+    owned_cards_inf = JSON.parse(localStorage.getItem("player_cards_inferno"));
+  }
+
+  /* In case we have data in our localstorage we display it */
+  if (localStorage.getItem("inferno_id")) {
+    inferno_deck.id = localStorage.getItem("inferno_id");
+    inferno_deck.remaining = localStorage.getItem("inferno_remaining");
+  } else {
+    /* In case we dont, we make api calls */
+    get_inferno_deck();
+  }
+});
+</script>
 
 <template>
   <div class="board">
@@ -9,12 +102,23 @@
           <h1 class="board__top-mazes__deck-box__deck-data__title">
             Inferno Deck
           </h1>
-          <p class="board__top-mazes__deck-box__deck-data__id">j1238193</p>
+          <p class="board__top-mazes__deck-box__deck-data__id">
+            {{ inferno_deck.id }}
+          </p>
         </div>
 
         <div class="board__top-mazes__deck-box__actions">
-          <button class="btn-action"><span>Shuffle</span></button>
-          <button class="btn-action"><span>Draw 2</span></button>
+          <button class="btn-action" @click="shuffle_cards(inferno_deck.id)">
+            <span>Shuffle</span>
+          </button>
+          <button
+            class="btn-action"
+            @click="
+              draw_card(inferno_deck, 'inferno_remaining', inferno_deck.id, 3)
+            "
+          >
+            <span>Draw 3</span>
+          </button>
         </div>
         <img
           class="board__top-mazes__deck-box__deck-img"
@@ -22,34 +126,18 @@
         />
         <div class="board__top-mazes__deck-box__stats">
           <label for="">Remaining Cards:</label>
-          <span class="board__top-mazes__deck-box__stats__number">56</span>
-        </div>
-      </div>
-      <!-- MAZO 2 -->
-      <div class="board__top-mazes__deck-box">
-        <div class="board__top-mazes__deck-box__deck-data">
-          <h1 class="board__top-mazes__deck-box__deck-data__title">
-            Citizen Deck
-          </h1>
-          <p class="board__top-mazes__deck-box__deck-data__id">j1238193</p>
-        </div>
-
-        <div class="board__top-mazes__deck-box__actions">
-          <button class="btn-action"><span>Shuffle</span></button>
-          <button class="btn-action"><span>Draw 2</span></button>
-        </div>
-        <img
-          class="board__top-mazes__deck-box__deck-img"
-          src="../assets/imgs/citizen.webp"
-        />
-        <div class="board__top-mazes__deck-box__stats">
-          <label for="">Remaining Cards:</label>
-          <span class="board__top-mazes__deck-box__stats__number">56</span>
+          <span class="board__top-mazes__deck-box__stats__number">{{
+            inferno_deck.remaining
+          }}</span>
         </div>
       </div>
     </div>
 
-    <div class="board__player-cards"></div>
+    <div class="board__player-cards">
+      <div class="cards" v-for="cards in owned_cards_inf">
+        <img class="cards__img" v-bind:src="cards.image" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -189,10 +277,15 @@
     align-items: center;
     justify-content: center;
 
-    .card-img {
-      height: 200px;
-      width: 150px;
-      margin-inline: 10px;
+    .cards {
+      &__img {
+        height: 200px;
+        margin-inline: 5px;
+
+        &:hover {
+          transform: scale(1.1);
+        }
+      }
     }
   }
 }
